ea2acbff260922742f18f62def54dcd2efdd2e19
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-06-26 23:01:13 +0200

    fix: add routes of component

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Header } from '@/presentation/components/Header';
 import { ThemeContainer } from '@/shared/styles';
+import { Routers } from '@/main/routes';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeContainer>
-      <Header />
+      <Routers />
     </ThemeContainer>
   </React.StrictMode>,
 );
