2d868cc830a5802d1eb9ab56ecf386ca4862029a
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-06-26 23:00:50 +0200

    feat: create home page

---
+++ src/presentation/page/Home/index.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+
+import { HomeContainer } from './styles';
+
+export const Home: FunctionComponent = () => {
+  return (
+    <HomeContainer>
+      <h2>Home</h2>
+    </HomeContainer>
+  );
+};
